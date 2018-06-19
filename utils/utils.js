cd5b3866715369b31c10050004e6fcebac273858
安第斯神鹰 <wall-e@live.cn>
2018-06-19 11:07:04 +0800

    feat(utils):新增样式转换

---
+++ utils/utils.js
@@ -0,0 +1,9 @@
+/**
+ * 将 object 转换为 style 样式（http://mpvue.com/mpvue/#class-style）
+ * @param obj
+ */
+export function toStyle (obj) {
+  let objStr = JSON.stringify(obj)
+  objStr = objStr.slice(1, objStr.length - 1).replace(/"/g, '')
+  return objStr.replace(',', ';')
+}
