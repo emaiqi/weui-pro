0e666ff4556c4cfc21a703d4c57f854845280956
安第斯神鹰 <wall-e@live.cn>
2018-06-19 17:54:44 +0800

    feat(flex):新增 flex 组件

---
+++ packages/flex-item/index.js
@@ -0,0 +1,7 @@
+import FlexItem from '../flex/flex-item.vue'
+
+FlexItem.install = function (Vue) {
+  Vue.component(FlexItem.name, FlexItem)
+}
+
+export default FlexItem
---
+++ packages/flex/flex.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="weui-flex" :style="style">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import {toStyle} from '../../utils/utils.js'
+
+  export default {
+    name: 'flex',
+    props: {
+      gutter: {
+        type: Number,
+        default: 8
+      },
+      orient: {
+        type: String,
+        default: 'horizontal'
+      },
+      wrap: String,
+      align: String,
+      justify: String,
+      direction: String
+    },
+    computed: {
+      style () {
+        const styles = {
+          'flex-wrap': this.wrap,
+          'align-items': this.align,
+          'justify-content': this.justify,
+          'flex-direction': this.direction
+        }
+        return toStyle(styles)
+      }
+    }
+  }
+</script>
