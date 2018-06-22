a9f7c339958ee0a8abb4295adf50322e3a30fca6
安第斯神鹰 <wall-e@live.cn>
2018-06-22 10:20:24 +0800

    feat(group-item): 新增 group-item 组件

---
+++ packages/group-item/index.js
@@ -0,0 +1,7 @@
+import GroupItem from '../group/group-item.vue'
+
+GroupItem.install = function (Vue) {
+  Vue.component(GroupItem.name, GroupItem)
+}
+
+export default GroupItem
---
+++ packages/group/group-item.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="weui-cell"  :class="{'weui-cell_access': showAccess, 'weui-cell_link': isLink, 'weui-cell_intent': intent}"
+       :hover-class="showAccess ? 'weui-cell_active' : ''">
+    <div class="weui-cell__hd" v-if="icon">
+      <img :src="icon" />
+    </div>
+    <div class="weui-cell__bd">
+      <p>{{title}}</p>
+    </div>
+    <div class="weui-cell__ft" :class="{'weui-cell__ft_in-access': showAccess}">
+      {{value}}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'cell',
+    props: {
+      icon: String,
+      title: [String, Number],
+      value: [Number, String],
+      intent: Boolean,
+      isLink: Boolean,
+      primary: Boolean,
+      showAccess: Boolean
+    }
+  }
+</script>
+
+<style lang="less">
+  .weui-cell_intent {
+    &:before {
+      left: 0;
+    }
+  }
+</style>
